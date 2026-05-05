b6cd81543243d01ba1c79b6d279d359a0f7dbeee
andrea-valerio <andrea@icio.it>
2026-05-05 16:18:15 +0200

    Update Hero component to reflect new portfolio version in header

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -62,7 +62,7 @@ export function Hero() {
                 WebkitTextFillColor: "transparent",
               }}
             >
-              PORTFOLIO
+              PORTFOLIO 22
             </span>
           </h1>
 
